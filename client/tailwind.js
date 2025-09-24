d8f30cabd3a80e8e0f02c13b5a9752b41eae68d7
leaderturk <leaderrturk@yahoo.com>
2025-09-24 11:42:46 +0300

    Update tailwind.js

--- client/tailwind.js
+++ client/tailwind.js
@@ -2,18 +2,21 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
       colors: {
-        background: {
-          DEFAULT: "#ffffff", // bg-background
-        },
-        border: {
-          DEFAULT: "#e5e7eb", // border-border
-        },
-      },
+        background: "#ffffff", // bg-background => beyaz
+        foreground: "#000000",
+        border: "#e5e7eb", // border-border => gri-200
+        card: "#ffffff",
+        "card-foreground": "#000000",
+        primary: "#3b82f6", // mavi
+        "primary-foreground": "#ffffff",
+        secondary: "#f3f4f6", // gri-100
+        "secondary-foreground": "#111827",
+      }
     },
   },
   plugins: [],
